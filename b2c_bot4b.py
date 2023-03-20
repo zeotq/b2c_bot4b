dac4d25b0640cc6565e7c7b4d9770214f6b74ee6
OKS5-14_z <OKS5-14@OKS5-14>
2023-03-20 11:29:24 +0300

    update

--- b2c_bot4b.py
+++ b2c_bot4b.py
@@ -30,20 +30,20 @@ class Form_Admin(StatesGroup):
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.reply('<b>Добро пожаловать!</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_main_menu)
+    await message.answer('<b>Добро пожаловать!</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_main_menu)
     user_data_save.data_writer(dict(message.from_user))
     await message.delete()
     await GlobalState.first()
 
 @dp.message_handler(commands=['menu'], state = "*")
 async def menu_command(message: types.Message):
-    await message.reply('<b>Главное меню</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_main_menu)
+    await message.answer('<b>Главное меню</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_main_menu)
     await message.delete()
     await GlobalState.first()
 
 @dp.message_handler(commands=['help'], state = "*")
 async def help_command(message: types.Message):
-   await message.reply(text="/menu - открыть меню,\n/close - закрыть")
+   await message.answer(text="/menu - открыть меню,\n/close - закрыть")
    await message.delete()
 
 @dp.message_handler(commands=['id'], state = "*")
@@ -55,14 +55,14 @@ async def admin_page_0(message: types.Message):
     user = users_db.db_get_user_by_id(message.from_user.id)
     if user.isAdmin():
         await Form_Admin.page_0.set()
-        await message.reply('<b>Открыта панель администратора</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_admin)
+        await message.answer('<b>Открыта панель администратора</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_admin)
     else:
-        await message.reply(text="Недостаточно прав.")
+        await message.answer(text="Недостаточно прав.")
         await message.delete()
 
 @dp.message_handler(commands=['exit', 'close'], state='*')
 async def close_menu_command(message: types.Message):
-    await message.reply(text='Главное меню', reply_markup = keyboards.keyboard_main_menu)
+    await message.answer(text='Главное меню', reply_markup = keyboards.keyboard_main_menu)
     await message.delete()
     await GlobalState.first()
 
@@ -110,18 +110,18 @@ async def taxi_reg(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         user = data[message.from_user.id]
         if user.isFull:
-            await message.reply(f'<b>Вы можете продожить или изменить данные</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg_finish)
+            await message.answer(f'<b>Вы можете продожить или изменить данные</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg_finish)
         if message.text == "Имя":
             await TaxiState.taxi_service_reg_name.set()
-            await message.answer("Введите имя", reply_markup = types.ReplyKeyboardRemove())
+            await message.answer("Введите имя:", reply_markup = types.ReplyKeyboardRemove())
         elif message.text == "Далее":
             user = data[message.from_user.id]
             print(user.get_user_data())
             if user.isFull():
                 user.write_user()
-                await message.reply(f'<b>Успешно!</b>\nИмя: {user.name}\nНомер телефона: {user.phone_number}', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_0)
+                await message.answer(f'<b>Успешно!</b>\nИмя: {user.name}\nНомер телефона: {user.phone_number}', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_0)
                 await state.finish()
-                await TaxiState.taxi_service_main
+                await TaxiState.taxi_service_main.set()
             else: pass
 
 @dp.message_handler(state=TaxiState.taxi_service_reg)
@@ -131,10 +131,10 @@ async def taxi_reg(message: types.Message, state: FSMContext):
             user = taxiuser(message.from_user.id)
             data[message.from_user.id] = user
             if user.isFull():
-                await message.reply(f'<b>Ваш аккаунт найден!</b>\nИмя: {user.name}\nНомер телефона: {user.phone_number}', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg_finish)
+                await message.answer(f'<b>Ваш аккаунт найден!</b>\nИмя: {user.name}\nНомер телефона: {user.phone_number}', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg_finish)
             else:
-                await message.reply(f'<b>Необходимо закончить регистрацию!</b>\nИмя: {user.name}\nНомер телефона: {user.phone_number}', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg)
-            await TaxiState.taxi_service_reg_main
+                await message.answer(f'<b>Необходимо закончить регистрацию!</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg)
+            await TaxiState.taxi_service_reg_main.set()
 
 @dp.message_handler(state=TaxiState.taxi_service_reg_name)
 async def taxi_reg(message: types.Message, state: FSMContext):
@@ -142,10 +142,10 @@ async def taxi_reg(message: types.Message, state: FSMContext):
         user = data[message.from_user.id]
         user.update(name = message.text)
         data[message.from_user.id] = user
-        await message.reply(f'<b>Получено новое имя - {user.name}</b>', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg_finish)
-        await TaxiState.taxi_service_reg.set()
+        await message.answer(f'Имя пользователя <b>{user.name}</b> сохранено', parse_mode="HTML", reply_markup = keyboards.keyboard_taxi_reg)
+        await TaxiState.taxi_service_reg_main.set()
 
-@dp.message_handler(state=TaxiState.taxi_service_reg_name, content_types=types.ContentType.CONTACT)
+@dp.message_handler(state=TaxiState.taxi_service_reg_main, content_types=types.ContentType.CONTACT)
 async def taxi_reg(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         user = data[message.from_user.id]
@@ -163,7 +163,7 @@ async def silkway(message: types.Message):
     elif text == "Информация о проекте":
         pass
     elif text == "Закрыть":
-        await message.reply(text='Открыть снова - /menu', reply_markup = types.ReplyKeyboardRemove())
+        await message.answer(text='Открыть снова - /menu', reply_markup = types.ReplyKeyboardRemove())
         await message.delete()
         await asyncio.sleep(delay = 5)
     else:
@@ -171,15 +171,6 @@ async def silkway(message: types.Message):
         "Я тебя не понял", "Команда не распознана", "Проверьте корректность сообщения", "Нет"
         ]
         await message.answer(text = choice(data))
-
-@dp.message_handler(state='*', commands=['send'])
-async def send(message: types.Message):
-    try:
-        service_data = message.text.split(" ")
-        format_message = message.text.replace(service_data[0]+" ", "").replace(service_data[1]+" ", "")
-        await bot.send_message(service_data[1], f"{message.from_user.full_name} \ {message.from_user.id}: {format_message}")
-    except:
-        await message.answer(text = 'Ошибка отправки сообщения')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
